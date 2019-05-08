7c9184059aa866dcfbf36bef008af32b2e5d2e84
rquintanam <itgeopanama@gmail.com>
2019-05-08 17:15:52 -0500

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+{
+	'name': 'DGI Partners Panamá',
+    'version': '1.0',
+    'category': 'Hidden/Dependency',
+    'description': """
+	Pestaña Partners Panama DGI
+	Parámetros para idendificar a los contribuyentes en la Rep. de Panamá
+	RUC, DV y Tipo de Persona
+	version: 2019-02-27
+""",
+    'author': 'Alconsoft-Sistech e ITGeo Panamá',
+    'website': 'http://alconsoft.net',
+    'depends': ['account',],
+    'init_xml':[],
+    'data': ['vista_partner_panama.xml'],
+    'demo': [],
+    'installable': True,
+    'application': True,
+}
---
+++ partner_panama.py
@@ -0,0 +1,28 @@
+﻿# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo
+#    Copyright (C) 2019 AlconSoft-Sistech (<http://alconsoft.net>).
+#
+##############################################################################
+
+from odoo import api, fields, models
+from odoo import tools
+
+class res_partner(models.Model):
+    _inherit = "res.partner"
+    _columns = {
+                # RUC: Registro Unico del Contribuyente (Panama)
+                'ruc': fields.char('RUC', size=45, required=True),
+                # DV: Digito verificador
+                'dv': fields.char('DV', size=2, required=True),
+                # TIPO_PERSONA
+                #  1=NATURAL 
+                #  2=JURIDICO
+                #  3=OTROS
+                #  4=EXTRANJERO
+                'tipo_persona': fields.selection([('1','NATURAL'),('2','JURIDICO'),('3','OTROS'),('4','EXTRANJERO')],'Tipo Persona',required=True),
+                # Concepto Varios a Uno
+                #'concepto' : fields.many2one('anexos.conceptos.pa', 'concepto','Anexo Concepto', store=True, readonly=False)
+                #'concepto' : fields.many2many('anexos.conceptos.pa','res_partner_anexos_conceptos_pa_rel','partner_id', 'concepto','Concepto de Anexo', store=True, readonly=False)
+               }
